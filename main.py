6370974cd4047f9cf34601d140555d8dfc5aa73f
greencrimea <tom.horton100@gmail.com>
2022-09-13 23:20:00 +1000

    .

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import retro
+import time
 
+#python -m retro.import .   <<<   IMPORT ROMS
+
+#open Street Fighter ROM
+env = retro.make(game='StreetFighterIISpecialChampionEdition-Genesis')
+
+#game loop
+obs = env.reset()
+done = False
+for game in range(1):
+    while not done:
+        if done:
+            obs = env.reset()
+        env.render()
+        obs, reward, done, info = env.step(env.action_space.sample())
+        print(reward)
+        
