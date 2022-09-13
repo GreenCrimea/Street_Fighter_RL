e94998c48ff1f0415af5bd05cf1436ea7a193563
greencrimea <tom.horton100@gmail.com>
2022-09-14 02:13:19 +1000

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
 import retro
-import time
 import numpy as np
 from gym.spaces import MultiBinary, Box 
 from gym import Env 
@@ -117,3 +117,33 @@ def optimize_ppo(trial):
     }
 
 #train and return mean reward
+def optimize_agent(trial):
+    #try:
+        model_params = optimize_ppo(trial)
+
+        #create environment
+        env = StreetFighter()
+        env = Monitor(env, LOG_DIR)
+        env = DummyVecEnv([lambda: env])
+        env = VecFrameStack(env, 4, channels_order='last')
+
+        #create model
+        model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=0, **model_params)
+        model.learn(total_timesteps=30000)
+
+        #evaluate model
+        mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=5)
+        env.close()
+
+        SAVE_PATH = os.path.join(OPT_DIR, 'trial_{}_BEST'.format(trial.number))
+        model.save(SAVE_PATH)
+
+        return mean_reward
+
+    #except Exception as e:
+    #    return -1000
+
+study = optuna.create_study(direction='maximize')
+study.optimize(optimize_agent, n_trials=10, n_jobs=1)
+
+print(study.best_params)
